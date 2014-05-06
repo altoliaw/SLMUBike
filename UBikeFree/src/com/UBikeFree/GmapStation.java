94a60e4669f9a157ce2feb54d051ad048bd1e0ea
altoliaw <altoliaw@hotmail.com>
2014-05-06 21:47:15 +0800

    嘗試寫一點triiger於GmapStation.java內
    還沒測試成功 (請繼續幫我測)  我吃藥了想睡覺

--- UBikeFree/src/com/UBikeFree/GmapStation.java
+++ UBikeFree/src/com/UBikeFree/GmapStation.java
@@ -1,17 +1,26 @@
 package com.UBikeFree;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
 import com.UBikeFree.R;
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.*;
 import com.StationInformation.Stations;
 import com.StationInformation.UBStation;
-
-import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+//import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.*;
+import static java.util.concurrent.TimeUnit.*;
 
 public class GmapStation extends FragmentActivity  {
  
     // Google Map
     private GoogleMap obj_GoogleMap;
+    private HashMap<String,Marker> obj_MarkInformation;
+    private final ScheduledExecutorService Obj_Trigger =Executors.newSingleThreadScheduledExecutor();
+
  
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,26 +48,52 @@ public class GmapStation extends FragmentActivity  {
         	// ROSE color icon        	
         	try{
 	        	Stations obj_Station =new Stations();
+	        	this.obj_MarkInformation=new HashMap<String,Marker>();
 	    		for(UBStation obj_UbiKeStation : obj_Station){
-	    			String str_Information=obj_UbiKeStation.getName() +":"+obj_UbiKeStation.getBikes()+"/"+obj_UbiKeStation.getEmptySlots();
-	    			MarkerOptions obj_Marker = new MarkerOptions().position(new LatLng(obj_UbiKeStation.getLat(),obj_UbiKeStation.getLng()))
-	    									.title(str_Information);
+	    			//.title(UBStationtemp.getName())
+                    //.snippet(("可借"+ UBStationtemp.getBikes()+ "空車位" + UBStationtemp.getEmptySlots()));	    			//
+	    			LatLng obj_Position=new LatLng(obj_UbiKeStation.getLat(),obj_UbiKeStation.getLng());
+	    			MarkerOptions obj_Mark = new MarkerOptions().position(obj_Position)
+	    									.title(obj_UbiKeStation.getName())
+	    									.snippet(("可借:"+ obj_UbiKeStation.getBikes()+ "/空車位:" + obj_UbiKeStation.getEmptySlots()))
+	    									.alpha(0.6F);	    			
+	    			String str_Key=String.valueOf(obj_UbiKeStation.getLat())+"/"+String.valueOf(obj_UbiKeStation.getLng());	    			
 	    			// ROSE color icon
-	    			obj_Marker.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+	    			obj_Mark.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
 	    			// adding marker
-	    			obj_GoogleMap.addMarker(obj_Marker);
+	    			Marker obj_MarkObject=obj_GoogleMap.addMarker(obj_Mark);
+	    			this.obj_MarkInformation.put(str_Key,obj_MarkObject);	    			    		
 	    		}
+	    		//this.ReloadData();	    		
         	}
         	catch(Exception obj_Ex){}
         }
     }
     
- 
+    //Trigger for reloading
+    public void ReloadData(){
+    	Runnable obj_Task=new Runnable(){
+    		public void run(){
+//    			Iterator obj_Iterate= obj_MarkInformation.entrySet().iterator(); 
+//    			while (obj_Iterate.hasNext()) { 
+//    			    Map.Entry entry = (Map.Entry) obj_Iterate.next(); 
+//    			    String str_Key = (String)entry.getKey(); 
+//    			    Marker obj_MarkerObject = (Marker)entry.getValue();
+//    			    String str_Id=obj_MarkerObject.getId();
+//    			    obj_MarkerObject.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+//    			} 
+    			System.out.println("beep");
+    		}    		
+    	};
+    	Obj_Trigger.schedule(obj_Task, 5, TimeUnit.SECONDS);
+    }
+    
     @Override
     protected void onResume() {
         super.onResume();
         this.InitilizeMap();
     }
- 
+    
+    
 }
 
